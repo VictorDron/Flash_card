40fb524dc106659bf4ecb6945f15727605cb4010
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-10-07 21:55:54 -0300

    Ok

---
+++ app.js
@@ -0,0 +1,3 @@
+var  law = [];
+law.push('Art. 1o  Esta Lei institui o Regime Jurídico dos Servidores Públicos Civis da União, das autarquias, inclusive as em regime especial, e das fundações públicas federais.');
+document.getElementById('show_law').innerText = law[0];
